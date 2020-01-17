b782e7e137ac722678f489ea06feea8790aa12ef
user <example@gmail.com>
2020-01-17 13:41:56 +0200

    carousel added

---
+++ src/js/script.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        // adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="/src/img/carousel/chevron-left-solid.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="/src/img/carousel/chevron-right-solid.png"></button>',
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+    });
+});
