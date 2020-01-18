f8d19053197d7ee22649c1a0d3323b2e0eb5d1e8
user <example@gmail.com>
2020-01-18 16:11:55 +0200

    modal added mobile adaptation added

--- src/js/script.js
+++ src/js/script.js
@@ -93,4 +93,22 @@ function toggleSlide(item){
 }
 
 toggleSlide('.catalog-item__link');
-toggleSlide('.catalog-item__back')
+toggleSlide('.catalog-item__back');
+
+//modal
+$(document).ready(function(){
+$('[data-modal=consultation]').on('click', function(){
+  $('.overlay, #consultation').fadeIn();
+});
+
+$('.modal__close').on('click', function(){
+  $('.overlay, #consultation, #thanks, #order').fadeOut('slow')
+});
+
+$('.button_mini').each(function(i){
+  $(this).on('click',function(){
+    $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+    $('.overlay, #order').fadeIn();
+  })
+})
+});
