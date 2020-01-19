cbad232b4dc3828ac9ca057ec8f4dc79064b481f
user <example@gmail.com>
2020-01-19 16:40:53 +0200

    site vaidation

--- src/js/script.js
+++ src/js/script.js
@@ -113,10 +113,12 @@ $(window).scroll(function(){
   }
 })
 
-$("a[href^='#']").click(function(){
+$("a[href='#up']").click(function(){
   var _href = $(this).attr("href");
   $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
   return false;
 });
+
+new WOW().init();
 
 });
